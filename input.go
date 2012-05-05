f6ddbdf573fc6cbf5e7945375af152659057f09b
Sergei Yu. Skorobogatov <skorobo@rambler.ru>
2012-05-05 14:32:57 +0400

    added support for all integer types, panics adjusted

--- input.go
+++ input.go
@@ -19,32 +19,54 @@ func Scanf(format string, a ...interface{}) (n int) {
 		}
 
 		if i == len(format) {
-			fmt := C.CString(string(buf))
-			n += int(C.scanverbatim(fmt))
-			C.free(unsafe.Pointer(fmt))
+			if len(buf) > 0 {
+				fmt := C.CString(string(buf))
+				n += int(C.scanverbatim(fmt))
+				C.free(unsafe.Pointer(fmt))
+			}
 			break
 		}
 
 		buf = append(buf, '%')
-		switch i++; format[i] {
-		case '%': buf = append(buf, '%')
-		case 'd':
-			if arg == len(a) {
-				panic("1 !!!")
-			} else if p, ok := a[arg].(*int); !ok {
-				panic("2 !!!")
-			} else {
-				buf = append(buf, 'd')
-				fmt := C.CString(string(buf))
-				var res C.int
-				n += int(C.scanint(fmt, &res))
-				C.free(unsafe.Pointer(fmt))
-				*p = int(res)
-				arg++
-				buf = buf[:0]
+		if i++; i == len(format) {
+			panic("input.Scanf: format string ends with '%'")
+		} else if format[i] == '%' {
+			buf = append(buf, '%')
+		} else {
+			spec := format[i]
+			if spec != 'd' && spec != 'c' {
+				panic("input.Scanf: only '%d' and '%c' format specifiers allowed")
 			}
-		}
 
+			if arg == len(a) {
+				panic("input.Scanf: the number of format specifiers exeeds the number of arguments")
+			}
+
+			buf = append(buf, spec)
+			fmt := C.CString(string(buf))
+			var res C.int
+			n += int(C.scanint(fmt, &res))
+			C.free(unsafe.Pointer(fmt))
+			buf = buf[:0]
+
+			switch p := a[arg].(type) {
+			case nil:
+				panic("input.Scanf: nil pointer passed as argument")
+			case *uint: *p = uint(res)
+			case *uint8: *p = uint8(res)
+			case *uint16: *p = uint16(res)
+			case *uint32: *p = uint32(res)
+			case *uint64: *p = uint64(res)
+			case *int: *p = int(res)
+			case *int8: *p = int8(res)
+			case *int16: *p = int16(res)
+			case *int32: *p = int32(res)
+			case *int64: *p = int64(res)
+			default:
+				panic("input.Scanf: argument must be pointer to some integer type")
+			}
+			arg++
+		}
 		i++
 	}
 
