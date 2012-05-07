6c7cb14c95ab82290a2559fe0aa86a8a0ee912e3
Sergei Yu. Skorobogatov <skorobo@rambler.ru>
2012-05-07 15:26:47 +0400

    try to disable some gcc format warnings

--- input.go
+++ input.go
@@ -4,7 +4,7 @@ package input
 // #include <stdlib.h>
 // #include "input.h"
 //
-// #cgo CFLAGS: -O3 -Wall -Wno-error=format-security
+// #cgo CFLAGS: -O3 -Wno-error=format-security -Wno-format-nonliteral -Wno-format-extra-args
 //
 import "C"
 
